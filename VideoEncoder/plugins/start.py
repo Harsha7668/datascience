222251c6ce0c7c7820145f7100c850b3b3f6f01e
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-27 21:37:15 +0530

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -47,7 +47,7 @@ async def start_message(app, message):
     if not c:
         return
     await AddUserToDatabase(app, message)
-    text = f"Hi {message.from_user.mention()}<a href='https://graph.org/file/49ab250d34f17b14ae559.jpg'>!</a> I'm VideoEncoder Bot which will do magic with your file."
+    text = f"Hi {message.from_user.mention()}<a href='https://telegra.ph/file/25c788ac5bc0a63040e6d.jpg'>!</a> I'm VideoEncoder Bot which will do magic with your file."
     await message.reply(text=text, reply_markup=start_but)
 
 
