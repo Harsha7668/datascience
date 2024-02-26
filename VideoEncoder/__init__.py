462a0c026782b7d15a5b199ee4b79e9103cb335b
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-02-26 14:48:25 +0530

    Update __init__.py

--- VideoEncoder/__init__.py
+++ VideoEncoder/__init__.py
@@ -77,10 +77,9 @@ PROGRESS = """
 
 if download_dir is not None and not os.path.isdir(download_dir):
     os.makedirs(download_dir)
-
-if not os.path.isdir(encode_dir):
+    
+if encode_dir is not None and not os.path.isdir(encode_dir):
     os.makedirs(encode_dir)
-
 
 logging.basicConfig(
     level=logging.DEBUG,
