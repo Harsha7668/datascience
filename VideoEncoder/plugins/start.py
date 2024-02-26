eb77d1bab16cae1a47328ba37a126bfbd3580313
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-02-26 15:31:03 +0530

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -27,7 +27,7 @@ async def start_message(app, message):
     if not check:
         return
     text = f"Hey! I'm <a href='https://telegra.ph/file/11379aba315ba245ebc7b.jpg'>VideoEncoder</a>. I can encode telegram files in x264.\n\nPress /help for my commands :)"
-    await message.reply(text=text,  disable_web_page_preview=True, reply_markup=start)
+    await message.reply(text=text, reply_markup=start_but)
 
 
 @Client.on_message(filters.command('help'))
@@ -43,7 +43,7 @@ async def help_message(app, message):
 • /sthumb - Save Thumb
 • /dthumb - Clear Thumb.
 • /logs - check logs."""
-    await message.reply(text=msg, disable_web_page_preview=True, reply_markup=output)
+    await message.reply(text=msg, disable_web_page_preview=True, reply_markup=start_but)
 
 
 @Client.on_message(filters.command('vset'))
@@ -63,7 +63,7 @@ Doc thumb: <code>{'True' if (doc_thumb) else 'False'}</code>
 <b>Sudo Users</b>
 <code>{sudo_users}</code>
 '''
-    await message.reply(text=text,  disable_web_page_preview=True, reply_markup=start)
+    await message.reply(text=text, reply_markup=start_but)
 
 
 @Client.on_message(filters.command('logs'))
