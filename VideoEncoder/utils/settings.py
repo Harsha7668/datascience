7ab7e0f5f7be12b317d32f07286a02c57afb2305
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-03-26 16:22:55 +0530

    Update settings.py

--- VideoEncoder/utils/settings.py
+++ VideoEncoder/utils/settings.py
@@ -91,9 +91,7 @@ async def VideoSettings(event: Message, user_id: int):
 
         r = await db.get_resolution(user_id)
         if r == 'OG':
-            res = 'Source'
-        elif r == '2160':
-            res = '2160p'   
+            res = 'Source'           
         elif r == '1080':
             res = '1080p'
         elif r == '720':
